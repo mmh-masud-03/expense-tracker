3d5febfaf15a731dd8d70c01a8f582c6fddd29d1
mmh-masud03 <2054901003@student.bup.edu.bd>
2024-07-05 15:48:16 +0600

    delete budget route

--- src/app/api/budget/[id]/delete.js
+++ src/app/api/budget/[id]/delete.js
@@ -1,17 +1,27 @@
 import Budget from "@/models/Budget";
 import { ConnectToDB } from "@/utils/connect";
+
 export const DELETE = async (req, { params }) => {
   try {
-    const { id } = params.id;
+    const { id } = params;
     await ConnectToDB();
     const deleteBudget = await Budget.findByIdAndDelete(id);
+
     if (!deleteBudget) {
-      return new Response(JSON.stringify("Error Deleting Budget"), {
-        status: 300,
+      return new Response(JSON.stringify({ error: "Budget not found" }), {
+        status: 404,
       });
     }
+
+    return new Response(
+      JSON.stringify({ message: "Budget deleted successfully" }),
+      {
+        status: 200,
+      }
+    );
   } catch (err) {
-    return new Response(JSON.stringify("Server Error"), {
+    console.error(err);
+    return new Response(JSON.stringify({ error: "Server error" }), {
       status: 500,
     });
   }
