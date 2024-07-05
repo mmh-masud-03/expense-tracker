5df8f6d4bdb6ae0a0efd7e68078b0c906f5496d2
mmh-masud03 <2054901003@student.bup.edu.bd>
2024-07-05 13:57:36 +0600

    create budget

---
+++ src/app/api/budget/create/route.js
@@ -0,0 +1,26 @@
+import { ConnectToDB } from "@/utils/connect";
+import Budget from "@/models/Budget";
+export const POST = async (req) => {
+  try {
+    await ConnectToDB();
+    const body = await req.json();
+    const { user, amount, month, year } = body;
+
+    if (!user || !amount || !month || !year) {
+      return new Response(
+        JSON.stringify({ error: "All fields are required" }),
+        {
+          status: 400,
+        }
+      );
+    }
+
+    const newBudget = await Budget.create({ user, amount, month, year });
+    return new Response(JSON.stringify(newBudget), { status: 201 });
+  } catch (err) {
+    console.error(err);
+    return new Response(JSON.stringify({ error: "Error creating budget" }), {
+      status: 500,
+    });
+  }
+};
