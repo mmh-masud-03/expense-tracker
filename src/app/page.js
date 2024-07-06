62b25e112b68a1906301d9d46fb3148be6323790
mmh-masud03 <2054901003@student.bup.edu.bd>
2024-07-06 14:15:57 +0600

    initial homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,14 @@
-export default async function Home() {
-  return <></>;
+// app/page.js
+import DashboardSummary from "@/components/DashboardSummary";
+import RecentTransactions from "@/components/RecentTransactions";
+import BudgetOverview from "@/components/BudgetOverview";
+
+export default function Home() {
+  return (
+    <div className="p-6">
+      <DashboardSummary />
+      <RecentTransactions />
+      <BudgetOverview />
+    </div>
+  );
 }
