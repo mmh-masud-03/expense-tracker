d989c903ba673037addef0b846c1e2e208d9dfdc
mmh-masud03 <2054901003@student.bup.edu.bd>
2024-07-27 23:54:00 +0600

    loading state in expense form

--- src/components/Expense/ExpenseForm.jsx
+++ src/components/Expense/ExpenseForm.jsx
@@ -9,6 +9,7 @@ export default function ExpenseForm({ expense, onClose }) {
   const [category, setCategory] = useState("");
   const [date, setDate] = useState("");
   const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
   const userId = getUserId();
 
   useEffect(() => {
@@ -25,6 +26,7 @@ export default function ExpenseForm({ expense, onClose }) {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setIsLoading(true);
     const data = {
       user: userId,
       title,
@@ -61,6 +63,8 @@ export default function ExpenseForm({ expense, onClose }) {
     } catch (error) {
       console.error("Error processing expense", error);
       setError("An unexpected error occurred");
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -184,7 +188,30 @@ export default function ExpenseForm({ expense, onClose }) {
           type="submit"
           className="flex-1 mr-2 py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-red-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition duration-150 ease-in-out"
         >
-          {expense ? "Update Expense" : "Add Expense"}
+          {isLoading ? (
+            <div className="flex justify-center items-center">
+              <svg className="animate-spin h-5 w-5 mr-3" viewBox="0 0 24 24">
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  strokeWidth="4"
+                />
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                />
+              </svg>
+              Processing...
+            </div>
+          ) : expense ? (
+            "Update Expense"
+          ) : (
+            "Add Expense"
+          )}
         </button>
         <button
           type="button"
