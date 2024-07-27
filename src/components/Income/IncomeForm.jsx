f2bbe9b9234d63c83b1d5e43da3461414d34ee84
mmh-masud03 <2054901003@student.bup.edu.bd>
2024-07-27 23:56:36 +0600

    loading state in income form

--- src/components/Income/IncomeForm.jsx
+++ src/components/Income/IncomeForm.jsx
@@ -10,6 +10,7 @@ export default function IncomeForm({ income, onClose }) {
   const [category, setCategory] = useState("");
   const [date, setDate] = useState("");
   const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
   const userId = getUserId();
   useEffect(() => {
     if (income) {
@@ -25,6 +26,7 @@ export default function IncomeForm({ income, onClose }) {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setIsLoading(true);
     const data = {
       user: userId, // Replace with dynamic user ID
       title,
@@ -59,6 +61,8 @@ export default function IncomeForm({ income, onClose }) {
     } catch (error) {
       console.error("Error processing income", error);
       setError("An unexpected error occurred");
+    } finally {
+      setIsLoading(false);
     }
   };
   return (
@@ -178,7 +182,30 @@ export default function IncomeForm({ income, onClose }) {
           type="submit"
           className="flex-1 mr-2 py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 transition duration-150 ease-in-out"
         >
-          {income ? "Update Income" : "Add Income"}
+          {isLoading ? (
+            <div className="flex justify-center items-center">
+              <svg className="animate-spin h-5 w-5 mr-3" viewBox="0 0 24 24">
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  strokeWidth="4"
+                />
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                />
+              </svg>
+              Processing...
+            </div>
+          ) : income ? (
+            "Update Income"
+          ) : (
+            "Add Income"
+          )}
         </button>
         <button
           type="button"
